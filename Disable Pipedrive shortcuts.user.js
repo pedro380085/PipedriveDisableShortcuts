467283deba274d9f0c5978ce797193c3d4ddac35
Pedro Goes <goes@inevent.com>
2022-07-06 23:23:44 -0400

    loop check billing issues

--- Disable Pipedrive shortcuts.user.js
+++ Disable Pipedrive shortcuts.user.js
@@ -14,16 +14,29 @@
   /**
    * Set listeners for shortcut event.
    */
-  ['keydown', 'keypress', 'keyup'].forEach(function(type){
-    window.addEventListener(type, function(event) {
-      console.log(event.target.nodeName);
-      console.log(event.type);
-      if (event.ctrlKey || event.metaKey) return;
-      if (event.target.nodeName != "BODY") return;
+	['keydown', 'keypress', 'keyup'].forEach(function(type) {
+		window.addEventListener(type, function(event) {
+			console.log(event.target.nodeName);
+			console.log(event.type);
+			if (event.ctrlKey || event.metaKey) return;
+			if (event.target.nodeName != "BODY") return;
 
-      event.stopImmediatePropagation();
-      event.preventDefault();
+			event.stopImmediatePropagation();
+			event.preventDefault();
 
-    }, true);
-  });
+		}, true);
+	});
+
+	window.addEventListener('load', function(event) {
+		setInterval(function () {
+			var visibleBlock = document.getElementsByClassName("cui4-message--visible");
+			if (visibleBlock.length > 0) {
+				var children = document.getElementsByClassName("cui4-message__close")[0].children;
+				console.log(children);
+				for (let element of children) {
+					element.click();
+				}
+			}
+		}, 2000);
+	}, true);
 })();
